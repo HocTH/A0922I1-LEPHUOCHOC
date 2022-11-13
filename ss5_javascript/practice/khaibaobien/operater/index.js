04c81db99819d371d11e361bf6254e40b0d70ded
TechCare <hoclephuoc1998@gmail.com>
2022-11-13 15:25:18 +0700

    new2

---
+++ ss5_javascript/practice/khaibaobien/operater/index.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputWidth);
+let area = width * height ;
+document.write("The area is :" + area);
